22e6a77fc362233ff16b4aef7d8349dab504dc54
lohitaksha <lohi1734@gmail.com>
2025-01-15 01:44:31 +0530

    subset1

---
+++ Recursion/subset1.cpp
@@ -0,0 +1,80 @@
+Given a array arr of integers, return the sums of all subsets in the list.  Return the sums in any order.
+
+Examples:
+
+Input: arr[] = [2, 3]
+Output: [0, 2, 3, 5]
+Explanation: When no elements are taken then Sum = 0. When only 2 is taken then Sum = 2. When only 3 is taken then Sum = 3. When elements 2 and 3 are taken then Sum = 2+3 = 5.
+Input: arr[] = [1, 2, 1]
+Output: [0, 1, 1, 2, 2, 3, 3, 4]
+Explanation: The possible subset sums are 0 (no elements), 1 (either of the 1's), 2 (the element 2), and their combinations.
+Input: arr[] = [5, 6, 7]
+Output: [0, 5, 6, 7, 11, 12, 13, 18]
+Explanation: The possible subset sums are 0 (no elements), 5, 6, 7, and their combinations.
+
+code:
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void fun(int idx,int sum,vector<int> &arr,vector<int> &res){
+        if(idx>=arr.size()){
+            res.push_back(sum);
+            return;
+        }
+        
+        sum+=arr[idx];
+        fun(idx+1,sum,arr,res);
+        sum-=arr[idx];
+        fun(idx+1,sum,arr,res);
+    }
+    vector<int> subsetSums(vector<int>& arr) {
+        // code here
+        vector<int> res;
+        fun(0,0,arr,res);
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+
+    while (t--) {
+        vector<int> inputArray;
+        string inputLine;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, inputLine);
+        stringstream inputStream(inputLine);
+        int num;
+        while (inputStream >> num) {
+            inputArray.push_back(num); // Read the array elements from input string
+        }
+
+        Solution solutionObject;
+        vector<int> result = solutionObject.subsetSums(inputArray);
+        sort(result.begin(), result.end());
+
+        for (int i = 0; i < result.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << result[i];
+        }
+        cout
+            << endl
+            << "~\n"; // Print results in list format with new line after each test case
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
