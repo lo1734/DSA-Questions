c11f4d6f3b67a1db96e2327e10eed60c331347ec
Lohitaksha <112050224+lo1734@users.noreply.github.com>
2025-01-15 09:54:20 +0530

    Update GenerateParantheses.cpp

--- Recursion/GenerateParantheses.cpp
+++ Recursion/GenerateParantheses.cpp
@@ -1,17 +1,17 @@
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
  
 
-Example 1:
+// Example 1:
 
-Input: n = 3
-Output: ["((()))","(()())","(())()","()(())","()()()"]
-Example 2:
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
 
-Input: n = 1
-Output: ["()"]
+// Input: n = 1
+// Output: ["()"]
 
-code:
+// code:
 
 class Solution {
 public:
