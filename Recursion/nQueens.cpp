60daf8d3d397bca447b0e5f342e6a73c15aa3843
Lohitaksha <112050224+lo1734@users.noreply.github.com>
2025-01-23 00:00:19 +0530

    Update nQueens.cpp

--- Recursion/nQueens.cpp
+++ Recursion/nQueens.cpp
@@ -1,8 +1,8 @@
-The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+// The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
 
-Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+// Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
 
-Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+// Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
 
 class Solution {
 public:
