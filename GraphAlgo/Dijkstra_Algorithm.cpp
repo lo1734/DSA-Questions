ca83723fd7505358a552dd4c4e6c3eb11947c37d
lohitaksha <lohi1734@gmail.com>
2025-02-01 21:13:17 +0530

    GA-3

---
+++ GraphAlgo/Dijkstra_Algorithm.cpp
@@ -0,0 +1,94 @@
+Given a weighted, undirected and connected graph where you have given adjacency list adj. You have to find the shortest distance of all the vertices from the source vertex src, and return a list of integers denoting the shortest distance between each node and source vertex src.
+
+Note: The Graph doesn't contain any negative weight edge.
+
+Examples:
+
+Input: adj = [[[1, 9]], [[0, 9]]], src = 0
+Output: [0, 9]
+Explanation:
+
+The source vertex is 0. Hence, the shortest distance of node 0 is 0 and the shortest distance from node 0 to 1 is 9.
+Input: adj = [[[1, 1], [2, 6]], [[2, 3], [0, 1]], [[1, 3], [0, 6]]], src = 2
+Output: [4, 3, 0]
+Explanation:
+
+For nodes 2 to 0, we can follow the path 2-1-0. This has a distance of 1+3 = 4, whereas the path 2-0 has a distance of 6. So, the Shortest path from 2 to 0 is 4.
+The shortest distance from 0 to 1 is 1 .
+
+Code:
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User Function Template
+class Solution {
+  public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex src.
+    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+        // Code here
+        int n=adj.size();
+        vector<int> dist(n,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,src});
+        dist[src]=0;
+        while(!pq.empty()){
+            int node=pq.top().second;
+            int dis=pq.top().first;
+            pq.pop();
+            for(auto it:adj[node]){
+                int nei=it.first;
+                int edwt=it.second;
+                if(dis+edwt<=dist[nei]){
+                    dist[nei]=dis+edwt;
+                    pq.push({dist[nei],nei});
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(dist[i]==INT_MAX) dist[i]=-1;
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<pair<int, int>>> adj(V);
+        int i = 0;
+        while (i++ < E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            pair<int, int> t1 = {v, w}, t2 = {u, w};
+            adj[u].push_back(t1);
+            adj[v].push_back(t2);
+        }
+        int src;
+        cin >> src;
+        cin.ignore();
+
+        Solution obj;
+        vector<int> res = obj.dijkstra(adj, src);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
