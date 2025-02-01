81144e5851b50bae9c199b34274cb9a30d0d04e5
lohitaksha <lohi1734@gmail.com>
2025-02-01 18:10:11 +0530

    GA-1

---
+++ GraphAlgo/Shortest_Path_in_Undirected.cpp
@@ -0,0 +1,78 @@
+You are given an adjacency list, adj of Undirected Graph having unit weight of the edges, find the shortest path from src to all the vertex and if it is unreachable to reach any vertex, then return -1 for that vertex.
+
+Examples :
+
+Input: adj[][] = [[1, 3], [0, 2], [1, 6], [0, 4], [3, 5], [4, 6], [2, 5, 7, 8], [6, 8], [7, 6]], src=0
+Output: [0, 1, 2, 1, 2, 3, 3, 4, 4]
+
+Code:
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Back-end complete function Template for C++
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the shortest path from source to all other nodes
+    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+        // code here
+        int n=adj.size();
+        vector<int> dist(n,INT_MAX);
+        queue<pair<int,int>> q;
+        q.push({src,0});
+        dist[src]=0;
+        while(!q.empty()){
+            int node=q.front().first;
+            q.pop();
+            for(auto it:adj[node]){
+                if(dist[node]+1<=dist[it]){
+                    dist[it]=dist[node]+1;
+                    q.push({it,dist[it]});
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(dist[i]==INT_MAX) dist[i]=-1;
+        }
+        return dist;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj(V);
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        int src;
+        cin >> src;
+
+        Solution obj;
+
+        vector<int> res = obj.shortestPath(adj, src);
+
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+        cout << "~\n";
+    }
+}
+
+// } Driver Code Ends
