da8c0bd040766660b56eec79eee7f62ad7e4fc8b
lohitaksha <lohi1734@gmail.com>
2025-02-01 19:09:02 +0530

    GA-2

---
+++ GraphAlgo/Shortest_path_in_Directed_Acyclic_Graph.cpp
@@ -0,0 +1,88 @@
+Given a Directed Acyclic Graph of V vertices from 0 to n-1 and a 2D Integer array(or vector) edges[ ][ ] of length E, where there is a directed edge from edge[i][0] to edge[i][1] with a distance of edge[i][2] for all i.
+
+Find the shortest path from src(0) vertex to all the vertices and if it is impossible to reach any vertex, then return -1 for that vertex.
+
+Examples :
+
+Input: V = 4, E = 2, edges = [[0,1,2], [0,2,1]]
+Output: [0, 2, 1, -1]
+Explanation: Shortest path from 0 to 1 is 0->1 with edge weight 2. Shortest path from 0 to 2 is 0->2 with edge weight 1. There is no way we can reach 3, so it's -1 for 3.
+Input: V = 6, E = 7, edges = [[0,1,2], [0,4,1], [4,5,4], [4,2,2], [1,2,3], [2,3,6], [5,3,1]]
+Output: [0, 2, 3, 6, 1, 5]
+Explanation: Shortest path from 0 to 1 is 0->1 with edge weight 2. Shortest path from 0 to 2 is 0->4->2 with edge weight 1+2=3. Shortest path from 0 to 3 is 0->4->5->3 with edge weight 1+4+1=6. Shortest path from 0 to 4 is 0->4 with edge weight 1.Shortest path from 0 to 5 is 0->4->5 with edge weight 1+4=5.
+
+Code:
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> shortestPath(int n, int E, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it:edges){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        vector<int> dist(n,INT_MAX);
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,0});
+        dist[0]=0;
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int n=pq.top().second;
+            pq.pop();
+            for(auto it:adj[n]){
+                int nei=it.first;
+                int edwt=it.second;
+                if(dis+edwt<=dist[nei]){
+                    dist[nei]=dis+edwt;
+                    pq.push({dist[nei],nei});
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(dist[i]==INT_MAX) dist[i]=-1;
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
