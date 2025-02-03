2c0648f6df1f405793d459e7b2a3c4553bf1d9a0
Lohitaksha <112050224+lo1734@users.noreply.github.com>
2025-02-03 17:16:45 +0530

    Update Dijkstra_Algorithm.cpp

--- GraphAlgo/Dijkstra_Algorithm.cpp
+++ GraphAlgo/Dijkstra_Algorithm.cpp
@@ -1,22 +1,22 @@
-Given a weighted, undirected and connected graph where you have given adjacency list adj. You have to find the shortest distance of all the vertices from the source vertex src, and return a list of integers denoting the shortest distance between each node and source vertex src.
+// Given a weighted, undirected and connected graph where you have given adjacency list adj. You have to find the shortest distance of all the vertices from the source vertex src, and return a list of integers denoting the shortest distance between each node and source vertex src.
 
-Note: The Graph doesn't contain any negative weight edge.
+// Note: The Graph doesn't contain any negative weight edge.
 
-Examples:
+// Examples:
 
-Input: adj = [[[1, 9]], [[0, 9]]], src = 0
-Output: [0, 9]
-Explanation:
+// Input: adj = [[[1, 9]], [[0, 9]]], src = 0
+// Output: [0, 9]
+// Explanation:
 
-The source vertex is 0. Hence, the shortest distance of node 0 is 0 and the shortest distance from node 0 to 1 is 9.
-Input: adj = [[[1, 1], [2, 6]], [[2, 3], [0, 1]], [[1, 3], [0, 6]]], src = 2
-Output: [4, 3, 0]
-Explanation:
+// The source vertex is 0. Hence, the shortest distance of node 0 is 0 and the shortest distance from node 0 to 1 is 9.
+// Input: adj = [[[1, 1], [2, 6]], [[2, 3], [0, 1]], [[1, 3], [0, 6]]], src = 2
+// Output: [4, 3, 0]
+// Explanation:
 
-For nodes 2 to 0, we can follow the path 2-1-0. This has a distance of 1+3 = 4, whereas the path 2-0 has a distance of 6. So, the Shortest path from 2 to 0 is 4.
-The shortest distance from 0 to 1 is 1 .
+// For nodes 2 to 0, we can follow the path 2-1-0. This has a distance of 1+3 = 4, whereas the path 2-0 has a distance of 6. So, the Shortest path from 2 to 0 is 4.
+// The shortest distance from 0 to 1 is 1 .
 
-Code:
+// Code:
 
 //{ Driver Code Starts
 #include <bits/stdc++.h>
